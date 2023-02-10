54897df3f2f46a142c7ebe29ca77ce3d13fa1600
Alena <renge.sa@mail.ru>
2023-02-10 14:14:55 +0400

    Task1

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int size = int.Parse(Console.ReadLine());
+string[] array1 = new string[size];
+string[] array2 = new string[array1.Length];
+
+    for (int i = 0; i < size; i++){
+	    Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	    array1[i] = result;  
+}
+
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(",", array1));
+Console.Write("]");
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+Console.WriteLine();
+Console.Write("Полученный массив: [");
+void PrintArray(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array1[i].Length <= 3){
+            array2[j] = array1[i];
+            Console.Write(array2[j] + ",");
+            j++;
+        }  
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
+
